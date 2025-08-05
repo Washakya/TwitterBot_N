fb8b9b678127af756ce7e75e9465d18e3fa4c1d1
Washakya <86008644+Washakya@users.noreply.github.com>
2025-08-05 23:21:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -108,4 +108,7 @@ result_img = api.media_upload(filename='sample2.png', file=img)
 #投稿
 if day != "8.10":
     client.create_tweet(text=name + "　#真夏の夜の淫夢", media_ids=[result_img.media_id])
+elif day == "8.7":
+    client.create_tweet(text=name + " #真夏の夜の淫夢", media_ids=[result_img.media_id])
+
 
